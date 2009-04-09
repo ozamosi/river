3bb5457d095be09d139c2b744c0b204495271657
Torbjörn Lönnemark <tobbez@ryara.net>
2009-04-10 01:52:12 +0200

    Add 'print' plugin

---
+++ river/plugins/print.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def new_entry (pdata, dl, item):
+    print 'Added download of %s' % item['title']
+
+def download_complete (pdata, dl, item):
+    print 'Saved %s to %s' % (item['title'], dl.get_save_path ())
+
+def download_started (pdata, dl, item):
+    print 'Download of %s started' % item['title']
+
+def download_update (pdata, dl, downloaded, length, item):
+    print 'Status update: %s. Downloaded: %s/%s' % (item['title'], str(downloaded), str(length))
+
+def init (pdata):
+    print 'Loaded simple print plugin'
+
+def shutdown (pdata):
+    print 'Unloaded simple print plugin'
